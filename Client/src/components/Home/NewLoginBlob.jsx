2f837ea9fd3b4cb42c1e1c0a9e5409dceb1856fd
Alberto Sierra <Sierra.Alberto@iCloud.com>
2024-01-09 19:56:45 -0800

    login to spotify

--- Client/src/components/Home/NewLoginBlob.jsx
+++ Client/src/components/Home/NewLoginBlob.jsx
@@ -5,12 +5,24 @@ const Blob = () => {
     {
       name: "Melodía",
       description: "Your personal music app",
-      url: "https://melodia.netlify.app/home",
+      // url: "https://melodia.netlify.app/home",
       username: "Sign in with Spotify!",
       className: "linkedin",
     },
   ];
+  const client_secret = import.meta.env.VITE_SPOTIFY_CLIENT_SECRET;
+  const client_id = import.meta.env.VITE_SPOTIFY_CLIENT_ID;
+  const redirect_uri = import.meta.env.VITE_SPOTIFY_REDIRECT_URI;
+  const scopes =
+    "user-read-playback-position user-read-playback-state user-top-read user-library-read user-library-modify user-read-private user-read-email user-read-currently-playing app-remote-control streaming playlist-read-private user-modify-playback-state playlist-modify-public playlist-modify-private";
 
+  const redirectToSpotifyLogin = () => {
+    const authUrl = `https://accounts.spotify.com/authorize?client_id=${client_id}&redirect_uri=${redirect_uri}&scope=${scopes.join(
+      "%20"
+    )}&response_type=code`;
+
+    window.location.href = authUrl;
+  };
   return (
     <div>
       {blobContent.map((social, index) => (
@@ -21,7 +33,7 @@ const Blob = () => {
           <div className="content">
             <h2>{social.name}</h2>
             <p>{social.description}</p>
-            <Link to="/home">{social.username}</Link>
+            <Link to={redirectToSpotifyLogin}>{social.username}</Link>
           </div>
         </div>
       ))}
